aa3b2e380dac5f4a3e10e9d01c943477046f9a13
grichardomi <grichardomi@gmail.com>
2021-12-15 02:33:54 +0000

    Develop html

---
+++ src/lib/todo-item.svelte
@@ -0,0 +1,16 @@
+<div>
+    <form action="" method="" >
+        <input type="hidden" name="Done" value="" />
+        <button area-label="Mark Done/Not Done"> DoneNotDone </button>
+    </form>
+
+    <form action="" method="" >
+        <input type="text" />
+        <button area-label="Save todo"> Save </button>
+    </form>
+
+    <form action="" method="" >
+        <button area-label="Delete a todo"> Delete </button>
+    </form>
+
+</div>
